1689bc561fbfd42c020f989b016defd909602315
AdrianOjeda <adrian.16300022@gmail.com>
2024-10-18 21:07:32 -0600

    Create new pieza controller done

--- Taller/src/main/java/gm/Taller/controlador/PiezaControlador.java
+++ Taller/src/main/java/gm/Taller/controlador/PiezaControlador.java
@@ -2,14 +2,14 @@ package gm.Taller.controlador;
 
 
 import gm.Taller.modelo.Piezas;
+import gm.Taller.repositorio.PiezaRepositorio;
 import gm.Taller.servicio.IPiezaServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,5 +32,16 @@ public class PiezaControlador {
         piezas.forEach((pieza -> logger.info(pieza.toString())));
 
         return piezas;
+    }
+
+    @PostMapping("/piezas")
+    public ResponseEntity<Piezas> createPieza(@RequestBody Piezas newPieza){
+        try{
+            Piezas savedPieza = PiezaServicio.savePieza(newPieza);
+            logger.info("Received request to create pieza: " + newPieza);
+            return new ResponseEntity<>(savedPieza, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR); // Return 500 error
+        }
     }
 }
